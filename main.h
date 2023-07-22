fe34737762ccf2eb09862b3eb86c6c8faf5d2369
Mukeli3 <mukelicynthia20@gmail.com>
2023-07-22 17:50:19 +0000

    tokenizing a string

--- main.h
+++ main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 /*header files*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stddef.h>
+#include <string.h>
 
 /*function prototypes*/
 char *get_line(void);
--- tokenizer.c
+++ tokenizer.c
@@ -1,4 +1,48 @@
 #include "main.h"
 
 /**
+ * tokenizer - function splits a string to tokens
+ * @str: string to tokenize
  *
+ * Return: pointer to an array
+ */
+char **tokenizer(char *str)
+{
+	int count = 0, i = 0;
+	char *token, *str1;
+	char **strptr;
+
+	token = strtok(str, " \n");
+
+	/* duplicate string for tokenizing */
+	str1 = strdup(str);
+
+	/* count the number of strings passed */
+	while (token)
+	{
+		token = strtok(NULL, " \n");
+		count++;
+	}
+
+	/* allocate memory for the counted number of tokens and a null pointer */
+	strptr = malloc(sizeof(char *) * (count + 1));
+	if (!strptr)
+	{
+		fprintf(stderr, "memory allocation failed");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(str1, " \n");
+
+	while (token)
+	{
+		/* store the duplicate token at the ith index */
+		strptr[i] = token;
+		token = strtok(NULL, " \n");
+		i++;
+	}
+	/* strptr is a null terminated string */
+	strptr[i] = NULL;
+	free(str1);
+	return (strptr);
+}
