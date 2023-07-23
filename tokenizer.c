c2edbb26700b0d736b76e6e8182eedf8de54687e
Mukeli3 <mukelicynthia20@gmail.com>
2023-07-23 00:21:02 +0000

    string tokenization

--- tokenizer.c
+++ tokenizer.c
@@ -12,17 +12,18 @@ char **tokenizer(char *str)
 	char *token, *str1;
 	char **strptr;
 
+	/* duplicate string for tokenizing */
+	str1 = strdup(str);
+
 	token = strtok(str, " \n");
 
-	/* duplicate string for tokenizing */
-	/* str1 = strdup(str); */
-
-	/* count the number of tokens passed */
+	/* count the number of tokens passed for memory allocation */
 	while (token)
 	{
 		token = strtok(NULL, " \n");
 		count++;
 	}
+	printf("%d\n", count);
 
 	/* allocate memory for the counted number of tokens and a null pointer */
 	strptr = malloc(sizeof(char *) * (count + 1));
@@ -31,18 +32,17 @@ char **tokenizer(char *str)
 		fprintf(stderr, "memory allocation failed");
 		exit(EXIT_FAILURE);
 	}
-
-	str1 = stdup(str);
 	token = strtok(str1, " \n");
 
 	while (token)
 	{
 		/* store the duplicate token at the ith index */
-		strptr[i] = token;
+		strptr[i] = strdup(token);
 		token = strtok(NULL, " \n");
 		i++;
 	}
-	/* strptr is a null terminated string */
+
+	/* strptr is a null terminated array of strings */
 	strptr[i] = NULL;
 	free(str1);
 	return (strptr);
