dfcbda37da839204956637e8f643b406339cb139
waicharikim <waicharikimani@gmail.com>
2023-07-26 03:28:36 -0700

    cd builtin

--- run_cd.c
+++ run_cd.c
@@ -1,1 +1,26 @@
 #include "main.h"
+
+/**
+ * run_cd - changes working directory
+ * args: target directory
+ *
+ * Return: 0 for Success -1 for failure
+ */
+int run_cd(char **args)
+{
+	char *env;
+
+	if (args[1] == NULL)
+	{
+		env = getenv("HOME");
+		chdir(env);
+		return (0);
+	}
+	else
+	{
+		if (chdir(args[1]) != 0)
+		    fprintf(stderr, "%s: not found\n", args[1]);
+		return (-1);
+	}
+	return (0);
+}
