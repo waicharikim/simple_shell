1cde1b7822f91d05168058d6f79133a05d24b41f
waicharikim <waicharikimani@gmail.com>
2023-07-26 16:05:37 +0300

    main shell file

--- main.c
+++ main.c
@@ -15,37 +15,35 @@ int main(int ac __attribute__((unused)), char *av[]__attribute__((unused)))
 		  if(write(STDOUT_FILENO, prompt, 2) == -1)
 			  exit(EXIT_FAILURE);
 
-		lineptr =  get_line();
-		args = tokenizer(lineptr);
+		  lineptr =  get_line();
+		  args = tokenizer(lineptr);
 
-		alias = isalias(args[0]);
-		if (alias)
-		{
-			isbuiltin(alias);
-			continue;
-		}
-		if (isbultin(args[0]) == -1)
-			cmd_path = ispath(args[0]);
-		if (cmd_path)
-		{
-			execve(
-		}
-		else
-			perror
-		i = 0;
-		while (args[i])
-		{
-			printf(-"%s\n", args[i]);
-			free(args[i]);
-			i++;
-		}
-		/* status = executor(args); */
-		free(lineptr);
-		free(args);
+		  alias = isalias(args[0]);
+		  if (alias)
+		  {
+			  isbuiltin(alias);
+			  continue;
+		  }
+		  if (isbultin(args[0]) == -1)
+			  cmd_path = ispath(args[0]);
+		  if (cmd_path)
+		  {
+			  execve();
+		  }
+		  else
+			  perror();
+		  i = 0;
+		  while (args[i])
+		  {
+			  printf(-"%s\n", args[i]);
+			  free(args[i]);
+			  i++;
+		  }
+		  /* status = executor(args); */
+		  free(lineptr);
+		  free(args);
 	  }
   }
-
   /*free(lineptr);*/
-	  return (-1);
+  return (-1);
 }
-
